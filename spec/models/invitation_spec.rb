c93022d0e4a6acf36f006a22014aeca85ae17420
Aditya Pandit <railswithaditya@gmail.com>
2023-06-07 16:57:42 +0530

    Added rspec test cases for user and invitation

---
+++ spec/models/invitation_spec.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Invitation, type: :model do
+  describe 'for enum status' do
+    it { should belong_to(:referrer).class_name('User') }
+  end
+
+  describe 'after_validation callback' do
+    let(:user) { FactoryBot.create(:user) }
+    let(:invitation) { FactoryBot.create(:invitation, referred_by: user.id) }
+
+    xit 'sends an invitation email if the record is valid' do
+      expect(invitation.status).to eq('pending')
+      invitation.valid?
+    end
+
+    xit 'does not send an invitation email if the record is invalid' do
+      expect(invitation).not_to receive(:send_invitation)
+      invitation.valid?
+    end
+  end
+
+  describe 'status enum' do
+    it 'defines the correct enum values' do
+      expect(Invitation.statuses).to eq({ 'pending' => 0, 'sent' => 1, 'accepted' => 2 })
+    end
+
+    it "has a default status of 'pending'" do
+      invitation = Invitation.new
+      expect(invitation.status).to eq('pending')
+    end
+  end
+
+  describe 'validations' do
+    it 'requires presence of email' do
+      invitation = Invitation.new
+      expect(invitation).to_not be_valid
+      expect(invitation.errors[:email]).to include("can't be blank")
+    end
+
+    it 'requires uniqueness of email' do
+      invitation = Invitation.new(email: 'test@example.com')
+      expect(invitation).to_not be_valid
+      expect(invitation.errors[:email]).to eq([])
+    end
+  end
+end
